6cf55c76499c9371e0db1576734b76d28acb49ea
Natalia Szymanska <natsza.ns@gmail.com>
2023-07-15 23:41:50 +0200

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 function titleClickHandler(event) {
+	const clickedElement = this;
 	console.log("Link was clicked!");
-	console.log(event);
 
 	/* remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll(".titles a.active");
@@ -12,14 +12,15 @@ function titleClickHandler(event) {
 	}
 
 	/* add class 'active' to the clicked link */
+	clickedElement.classList.add("active");
+	console.log("clickedElement:", clickedElement);
 
+	/* remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll(".posts article.active");
 
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove("active");
 	}
-
-	/* remove class 'active' from all articles */
 
 	/* get 'href' attribute from the clicked link */
 
