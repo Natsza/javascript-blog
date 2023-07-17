0e9e25e251d1c700709a005c0bcf11994d72e0fb
Natalia Szymanska <natsza.ns@gmail.com>
2023-07-17 15:03:03 +0200

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -24,9 +24,18 @@ function titleClickHandler(event) {
 
 	/* get 'href' attribute from the clicked link */
 
+	const articleSelector = clickedElement.getAttribute("href");
+
+	console.log("articleSelector:", articleSelector);
+
 	/* find the correct article using the selector (value of 'href' attribute) */
 
+	const targetArticle = document.querySelector(articleSelector);
+	console.log("targetArticle:", targetArticle);
+
 	/* add class 'active' to the correct article */
+	targetArticle.classList.add("active");
+	console.log("targetArticle:", targetArticle);
 }
 
 const links = document.querySelectorAll(".titles a");
